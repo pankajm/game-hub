c3f0818d75c5834cf0ca58871a1c485bfcf428c4
Pankaj Marghade <p.g.marghade@gmail.com>
2024-01-10 20:00:33 +0530

    Added criticscore

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,13 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const color = score > 90 ? "green" : score > 60 ? "yellow" : "";
+
+  return <Badge colorScheme={color}>{score}</Badge>;
+};
+
+export default CriticScore;
