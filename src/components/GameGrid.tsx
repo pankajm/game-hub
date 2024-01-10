a6847d6a7dd8b642e36634682be728cd874eff85
Pankaj Marghade <p.g.marghade@gmail.com>
2024-01-10 23:34:17 +0530

    Refactor: Wrapped Gamecard and GameCardSkeleton with GameCardContainer

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GamesCardSkeleton from "./GamesCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
@@ -10,9 +11,16 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={10} padding={2}>
-        {isLoading && cards.map((card) => <GamesCardSkeleton key={card} />)}
+        {isLoading &&
+          cards.map((card) => (
+            <GameCardContainer>
+              <GamesCardSkeleton key={card} />
+            </GameCardContainer>
+          ))}
         {games.map((g) => (
-          <GameCard key={g.id} game={g}></GameCard>
+          <GameCardContainer>
+            <GameCard key={g.id} game={g}></GameCard>
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
