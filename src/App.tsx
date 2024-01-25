d240713127d3ad8f5b56ec85162cfbb200daf570
Pankaj Marghade <p.g.marghade@gmail.com>
2024-01-25 14:56:43 +0530

    UI fix : Added top margin for Heading

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <Box paddingLeft={2}>
+        <Box paddingLeft={2} marginTop={5}>
           <DynamicHeading
             genre={gameQuery.genre?.name}
             platform={gameQuery.platform?.name}
